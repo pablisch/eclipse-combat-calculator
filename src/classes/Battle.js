3984724226d28f03c2cb76116722b9a92793f578
Pablo <pablito@me.com>
2024-02-03 00:46:09 +0000

    Implented the new Battle class fully into App.jsx and all appears to be well.

--- src/classes/Battle.js
+++ src/classes/Battle.js
@@ -28,6 +28,7 @@ export default class Battle {
     );
     this.targetRole = shooters[0].role === 'attacker' ? 'defender' : 'attacker';
     this.shooters = shooters;
+    shooters.forEach((shooter) => shooter.setRounds());
   }
 
   getCannonRoundShots() {
@@ -51,7 +52,7 @@ export default class Battle {
     if (this.sameShieldValue()) {
       // console.log(this.targets[0]);
       const adjustedShots = this.shots.filter(
-        (shot) => shot.value - this.targets[0].shields >= 6 || shot.six
+        (shot) => shot.value - this.targets[0]?.shields >= 6 || shot.six
       );
       // console.log('shots:', this.shots, 'adjusted:', adjustedShots);
       this.shots = adjustedShots;
@@ -59,34 +60,34 @@ export default class Battle {
   }
 
   assignCannonDamageToTargetWhenEqualShieldValues() {
-    console.log('shots:', this.shots);
+    // console.log('shots:', this.shots);
     while (this.shots.length > 0 && this.targets.length > 0) {
       const target = this.targets[0];
-      console.log(' - target:', target.name, 'hulls:', target.hulls)
+      // console.log(' - target:', target.name, 'hulls:', target.hulls)
       const index1 = this.indexOfShotWithDamage(1);
       const index2 = this.indexOfShotWithDamage(2);
       if (target.hulls === 1 && index1 !== -1) {
-        console.log(`💥 Hit and destroyed ${target.name} with 1️⃣  damage`)
+        // console.log(`💥 Hit and destroyed ${target.name} with 1️⃣  damage`)
         this.shots.splice(index1, 1);
         this.destroyShip(target.id);
         this.targets.shift();
       } else if (target.hulls === 2 && index2 !== -1) {
-        console.log(`💥 Hit and destroyed ${target.name} with 2️⃣  damage`)
+        // console.log(`💥 Hit and destroyed ${target.name} with 2️⃣  damage`)
         this.shots.splice(index2, 1);
-          this.destroyShip(target.id);
-          this.targets.shift();
+        this.destroyShip(target.id);
+        this.targets.shift();
       } else {
-        console.log(`🚀 Hit ${target.name} with ${this.shots[0].damage} damage`)
+        // console.log(`🚀 Hit ${target.name} with ${this.shots[0].damage} damage`)
         target.removeHulls(this.shots[0].damage);
         if (target.hulls <= 0) {
-          console.log(`... and destroyed ${target.name} 🤯`)
+          // console.log(`... and destroyed ${target.name} 🤯`)
           this.destroyShip(target.id);
           this.targets.shift();
-        } 
+        }
         this.shots.shift();
       }
     }
-    console.log('shots:', this.shots, 'targets:', this.targets);
+    // console.log('shots:', this.shots, 'targets:', this.targets);
   }
 
   indexOfShotWithDamage(damageValue) {
@@ -96,6 +97,27 @@ export default class Battle {
   destroyShip(id) {
     this.armies = this.armies.filter((ship) => ship.id !== id);
   }
+
+    battleOver() {
+      const uniqueRoles = [...new Set(this.armies.map((ship) => ship.role))];
+      return uniqueRoles.length === 1;
+  }
+  
+  findWinner() {
+    return this.armies[0].role
+  }
+
+  fight() {
+    while (!this.battleOver()) {
+      // console.log('battle is over:', this.battleOver());
+      this.getNextShooters();
+      this.getCannonRoundShots();
+      this.getTargets();
+      this.adjustShotsForEqualShieldValues();
+      this.assignCannonDamageToTargetWhenEqualShieldValues();
+    }
+    return this.findWinner();
+  }
 }
 
 // const ship1 = new Ship('attacker', 'Cruiser', 3, 5, 4, 0, 0, 5, 0, 1);
@@ -103,15 +125,19 @@ export default class Battle {
 // const ship5 = new Ship('attacker', 'Dreadnought', 3, 3, 0, 3, 0, 0, 5, 2);
 // const ship3 = new Ship('defender', 'Cruiser', 3, 2, 2, 3, 7, 0, 0, 3);
 // const ship4 = new Ship('defender', 'Dreadnought', 3, 1, 2, 3, 0, 5, 0, 2);
-// const battle = new Battle([ship1, ship2, ship3, ship4, ship5]);
+// // const battle = new Battle([ship1, ship2, ship3, ship4, ship5]);
+// const battle = new Battle([ship4]);
 // // // ship3.setRounds();
 // // // ship1.setRounds();
 // // ship4.setRounds();
 // // // ship2.setRounds();
 // battle.getNextShooters();
 // battle.getCannonRoundShots();
-// // console.log(battle.shots);
-// // // console.log(battle.getTargets());
+// console.log(battle.shots);
+// // console.log(battle.getTargets());
 // battle.getTargets();
 // battle.adjustShotsForEqualShieldValues();
 // battle.assignCannonDamageToTargetWhenEqualShieldValues();
+
+// console.log(battle.battleOver());
+// console.log(battle.findWinner());
